e2a02f0ef33d2527181dca48de3b6d45946d8d30
Mourvanhmaes <mourvanhmaes@gmail.com>
2025-09-07 21:31:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function buscarPokemon(){
         let imagem = dados.sprites.front_default;
         if(tipos.includes(elemento) || elemento == "nada" || elemento == null){
             pokemon.innerHTML += `
-                <div class="card">
+                <div class="card" onclick="abrir_pokemon(${id})">
                     <img src="${imagem}" alt="${nome}">
                     <h6>#${id}</h6>
                     <h3>${nome.charAt(0).toUpperCase() + nome.slice(1)}</h3>
@@ -85,7 +85,7 @@ async function buscar(){
     let tipos = dados.types.map(t => t.type.name);
     let imagem = dados.sprites.front_default;
     pokemon.innerHTML += `
-        <div class="card">
+        <div class="card" onclick="abrir_pokemon(${id})">
             <img src="${imagem}" alt="${nome}">
             <h6>#${id}</h6>
             <h3>${nome.charAt(0).toUpperCase() + nome.slice(1)}</h3>
@@ -103,4 +103,62 @@ document.getElementById("pesquisa").addEventListener("keydown", function(m_enter
       buscar();
 }
 });
+
+async function abrir_pokemon(id){
+    let resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    let dados = await resposta.json();
+    let nome = dados.name;
+    let tipos = dados.types.map(t => t.type.name);
+    let imagem = dados.sprites.front_default;
+    let resposta2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    let altura = dados.height / 10;
+    let peso = dados.weight / 10;
+    let habilidades = dados.abilities.map(h => h.ability.name);
+    let dados2 = await resposta2.json();
+    let movimentos = dados.moves.map(m => m.move.name);
+
+    let descricao = dados2.flavor_text_entries.find(
+        entry => entry.language.name === "en"
+    ).flavor_text;
+    
+    let poke = {
+        id,
+        nome,
+        tipos,
+        imagem,
+        altura,
+        peso,
+        habilidades,
+        movimentos,
+        descricao
+    };
+    localStorage.setItem("pokemon", JSON.stringify(poke));
+
+    // Redireciona
+     window.location.href = "pokemon.html";
+    // let poke = document.getElementById("pag_poke");
+    // poke.innerHTML += `
+    //     <div class="texto">
+    //         <div class="poke_nome">
+    //             <h1>${nome}<span>(#${id})</span></h1>
+    //             ${tipos.map(t => `<h3>${t}</h3>`).join('')}
+    //         </div>
+    //         <div class="poke_img">
+    //             <img src="${imagem}" alt="${nome}">
+    //             <div class="dados">
+    //                 <p class="descricao">${descricao}</p>
+    //                 <div class="info">
+    //                     <h3>Altura: <span>${altura} m</span></h3>
+    //                     <h3>Peso: <span>${peso} kg</span></h3>
+    //                     <h3>Habilidade: <span>${habilidades}</span></h3>    
+    //                 </div>
+    //                 <div class="moves">
+    //                     <h3>Movimentos:${movimentos.map(m => `<span>${m}</span>`).join('')}</h3>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // `;
+}
+
 buscarPokemon();
